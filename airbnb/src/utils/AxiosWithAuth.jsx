93ebbb533a29de238871a4f6ed71631fcee5be36
Anthony Sutton <anthony.sutton95@gmail.com>
2019-11-18 11:36:47 -0800

    AxiosWithAuth can handle CORS issue with using Heroku

--- airbnb/src/utils/AxiosWithAuth.jsx
+++ airbnb/src/utils/AxiosWithAuth.jsx
@@ -4,8 +4,10 @@ const AxiosWithAuth = () => {
   const token = sessionStorage.getItem('token');
 
   return axios.create({
-    baseURL: 'https://airbnb-optimal-price-app.herokuapp.com',
-    headers: {authorization: token}
+    baseURL: 'https://cors-anywhere.herokuapp.com/https://airbnb-optimal-price-app.herokuapp.com',
+    headers: {authorization: token,
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'GET,PUSH,POST,PATCH,DELETE,OPTIONS'}
   })
 };
 
