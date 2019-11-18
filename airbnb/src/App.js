d5c5edb6c863de543a1d5e235e67bafcbf387c74
B Severino <biancasev@gmail.com>
2019-11-18 15:35:16 -0500

    Fixed dependency location

--- airbnb/src/App.js
+++ airbnb/src/App.js
@@ -2,10 +2,17 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 import './App.css';
 
+import LogIn from './components/LogIn';
+
 function App() {
   return (
     <div className="App">
-
+        <Route exact path ='/' component={LogIn} />
+        <Route path='/register' component={NewUser} />
+        <Route exact path='/user' component={UserProfile} />
+        <Route path='/user/edit' component={UserEdit} />
+        <Route exact path='/listing/id' component={Listing} />
+        <Route path='/listing/id/edit' component={ListingEdit} />
     </div>
   );
 }
