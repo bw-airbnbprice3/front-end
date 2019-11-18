696c8d5d3fd9ae85c86b2f164134f3d6166b7c8c
Anthony Sutton <anthony.sutton95@gmail.com>
2019-11-18 12:14:05 -0800

    Fixed spelling error in file

--- airbnb/src/utils/PrivateRoute.jsx
+++ airbnb/src/utils/PrivateRoute.jsx
@@ -3,7 +3,7 @@ import {Route, Redirect} from "react-router-dom";
 
 const PrivateRoute = ({component: Component, ...rest}) => {
   return (
-    <Route {...reset} render={props => {
+    <Route {...rest} render={props => {
       if (sessionStorage.getItem('token')) {
         return <Component {...props}/>
       } else {
