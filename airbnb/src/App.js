609384f28ba4787eb17095ceaaed0d7a83e5b0d2
Anthony Sutton <anthony.sutton95@gmail.com>
2019-11-18 16:58:17 -0800

    Fixed Routing Issues

--- airbnb/src/App.js
+++ airbnb/src/App.js
@@ -19,15 +19,14 @@ function App() {
       {/*Private routes can be added here. Just follow the basic template below. This will automatically check if the token is saved in storage. If not, it will redirect them to the login page*/}
       <Switch>
         <PrivateRoute path={"/listings"} component={ViewAllListings}/>
+        <PrivateRoute path={"/user/edit"} component={UserEdit} />
+        <PrivateRoute path='/listing/add' component={ListingAdd} />
+        <PrivateRoute exact path='/listing/id' component={Listing} />
+        <PrivateRoute path='/listing/id/edit' component={ListingEdit} />
+        <PrivateRoute exact path='/user' component={UserProfile} />
       </Switch>
 
       {/* <Route path='/register' component={NewUser} /> */}
-      <Route exact path='/user' component={UserProfile} />
-      <Route path='/user/edit' component={UserEdit} />
-      {/* <Route exact path='/listings' component={Listings} /> */}
-      <Route path='/listings/add' component={ListingAdd} />
-      <Route exact path='/listings/id' component={Listing} />
-      <Route path='/listings/id/edit' component={ListingEdit} />
 
     </div>
   );
