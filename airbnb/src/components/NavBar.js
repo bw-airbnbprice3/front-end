4af5eaa6b3e2e1b76e4a71769e23a9d39c56c0ec
Anthony Sutton <anthony.sutton95@gmail.com>
2019-11-19 13:44:56 -0800

    Removes private routes from navbar if the user is not logged in

--- airbnb/src/components/NavBar.js
+++ airbnb/src/components/NavBar.js
@@ -3,36 +3,42 @@ import {AppBar, Toolbar, Button} from '@material-ui/core';
 
 const NavBar = props => {
 
-    // destroys the token and the username session that is being stored and redirects the user to the login page.
-    const destroySessionStorage = () => {
-      sessionStorage.removeItem('token');
-      sessionStorage.removeItem('username');
-      props.history.push("/");
-    };
+  // destroys the token and the username session that is being stored and redirects the user to the login page.
+  const destroySessionStorage = () => {
+    sessionStorage.removeItem('token');
+    sessionStorage.removeItem('username');
+    props.history.push("/");
+  };
 
-    return (
-        <AppBar position="static">
-            <Toolbar>
-                <nav className="nav-bar">
-                    <div>
-                        <Button color="inherit" onClick={() => props.history.push("/listings")}>View Listings</Button>
-                        <Button color="inherit" onClick={() => props.history.push("/listing/add")}>Add New Listing</Button>
-                        <Button color="inherit" onClick={() => props.history.push("/user")}>User Profile</Button>
-                    </div>
-                    {sessionStorage.getItem('token') ?
-                        <div>
-                            <Button color="inherit" onClick={destroySessionStorage}>Log Out</Button>
-                        </div>
-                    : 
-                        <div>
-                            <Button color="inherit" onClick={() => props.history.push("/")}>Log In</Button>
-                            <Button color="inherit" onClick={() => props.history.push("/register")}>Register</Button>
-                        </div>
-                    }
-                </nav>
-            </Toolbar>
-        </AppBar>
-    )
+  return (
+    <AppBar position="static">
+      <Toolbar>
+        <nav className="nav-bar">
+
+          {sessionStorage.getItem('token') ?
+            <>
+              <div>
+                <Button color="inherit" onClick={() => props.history.push("/listings")}>View Listings</Button>
+                <Button color="inherit" onClick={() => props.history.push("/listing/add")}>Add New Listing</Button>
+                <Button color="inherit" onClick={() => props.history.push("/user")}>User Profile</Button>
+              </div>
+              <div>
+                <Button color="inherit" onClick={destroySessionStorage}>Log Out</Button>
+              </div>
+            </>
+            :
+            <>
+            <div />
+            <div>
+              <Button color="inherit" onClick={() => props.history.push("/")}>Log In</Button>
+              <Button color="inherit" onClick={() => props.history.push("/register")}>Register</Button>
+            </div>
+            </>
+          }
+        </nav>
+      </Toolbar>
+    </AppBar>
+  )
 };
 
 export default NavBar;
