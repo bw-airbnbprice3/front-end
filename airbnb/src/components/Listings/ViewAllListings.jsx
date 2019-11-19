a72b1c79eaa9cdc52be039faa05046269ed01b5a
Corey Gumbs <5513859+CoreyGumbs@users.noreply.github.com>
2019-11-19 18:52:35 -0500

    Added material ui to styling of listings

--- airbnb/src/components/Listings/ViewAllListings.jsx
+++ airbnb/src/components/Listings/ViewAllListings.jsx
@@ -1,5 +1,6 @@
 import React, {useState, useEffect} from "react";
 import {Link} from "react-router-dom";
+import {Container, Card, Typography, Link as MaterialLink} from '@material-ui/core';
 import AxiosWithAuth from "../../utils/AxiosWithAuth";
 
 const ViewAllListings = () => {
@@ -9,25 +10,35 @@ const ViewAllListings = () => {
     AxiosWithAuth().get('api/listings/').then(response => {
       console.log(response.data);
       setListings(response.data);
-    
     })
-      .catch(error => console.log(error));
+    .catch(error => console.log(error));
    
   }, []);
   // Need to map over all of the listings here
   return (
     <div>
-      <h1>Listings</h1>
-      <div className="allListings">
-
-        {/* map listings here; sample card below */}
-        <div className="listingCard">
-          <h3>Property Name</h3>
-          <p>Location</p>
-          {/* <Link to="/listing/id">View Listing</Link> */}
-        </div>
-
-      </div>
+      <h1>Current Listings</h1>
+      {
+        listings.map(listing => (
+          <Card key={listing.id}>
+            <Link to={`/listing/${listing.id}`}>
+            <Typography variant="h3">
+              <MaterialLink>
+              {listing.property_name}
+              </MaterialLink>
+            </Typography>
+            <Typography variant="h4">{listing.neighborhood}</Typography>
+            </Link>
+            <div>
+              <Link to={`/listing/${listing.id}/edit`}>
+              <button>Edit</button>
+              </Link>
+              <button>Delete</button>
+            </div>
+          
+          </Card>     
+        ))
+      }
     </div>
 
   );
