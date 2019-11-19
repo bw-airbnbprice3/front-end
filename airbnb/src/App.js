dea1fad96bab7baf67288e1720900a293d8f255c
Anthony Sutton <anthony.sutton95@gmail.com>
2019-11-18 16:52:00 -0800

    Merged from master

--- airbnb/src/App.js
+++ airbnb/src/App.js
@@ -3,9 +3,13 @@ import {Route, Switch} from "react-router-dom";
 import PrivateRoute from "./utils/PrivateRoute";
 import "./App.css";
 
-<<<<<<< HEAD
 import LogInForm from "./components/LogIn";
 import ViewAllListings from "./components/Listings/ViewAllListings";
+import UserProfile from './components/UserProfile';
+import UserEdit from './components/UserEdit';
+import ListingAdd from './components/ListingAdd';
+import Listing from './components/Listing';
+import ListingEdit from './components/ListingEdit';
 
 function App() {
   return (
@@ -16,24 +20,7 @@ function App() {
       <Switch>
         <PrivateRoute path={"/listings"} component={ViewAllListings}/>
       </Switch>
-      {/* <Route path='/register' component={NewUser} />
-          <Route exact path='/user' component={UserProfile} />
-          <Route path='/user/edit' component={UserEdit} />
-          <Route exact path='/listing/id' component={Listing} />
-          <Route path='/listing/id/edit' component={ListingEdit} /> */}
-=======
-import LogInForm from './components/LogIn';
-import UserProfile from './components/UserProfile';
-import UserEdit from './components/UserEdit';
-import ListingAdd from './components/ListingAdd';
-import Listing from './components/Listing';
-import ListingEdit from './components/ListingEdit';
 
-function App() {
-  return (
-    <div className="App">
-
-      <Route exact path="/" component={LogInForm} />
       {/* <Route path='/register' component={NewUser} /> */}
       <Route exact path='/user' component={UserProfile} />
       <Route path='/user/edit' component={UserEdit} />
@@ -41,7 +28,7 @@ function App() {
       <Route path='/listings/add' component={ListingAdd} />
       <Route exact path='/listings/id' component={Listing} />
       <Route path='/listings/id/edit' component={ListingEdit} />
->>>>>>> 32268b991b0c8baf21b5227dca1051871b0786ce
+
     </div>
   );
 }
