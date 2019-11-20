6c5569d954ea87fbfce32d7951d43dcd342f1298
Anthony Sutton <anthony.sutton95@gmail.com>
2019-11-20 08:42:39 -0800

    Updated some more actions

--- airbnb/src/reducers/index.jsx
+++ airbnb/src/reducers/index.jsx
@@ -1,14 +1,14 @@
-import {FETCH_START, FETCH_SUCCESS, FETCH_FAILURE} from "../actions"
+import {FETCH_LISTINGS_START, FETCH_LISTINGS_SUCCESS, FETCH_LISTINGS_FAILURE} from "../actions"
 
 const initialState = {listingData: [], isFetching: false, errors: null};
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
-    case FETCH_START:
+    case FETCH_LISTINGS_START:
       return {...state, listingData: [], isFetching: true, errors: null};
-    case FETCH_SUCCESS:
+    case FETCH_LISTINGS_SUCCESS:
       return {...state, listingData: action.payload, isFetching: false, errors: null};
-    case FETCH_FAILURE:
+    case FETCH_LISTINGS_FAILURE:
       return {...state, listingData: [], isFetching: false, errors: action.payload};
     default:
       return state;
