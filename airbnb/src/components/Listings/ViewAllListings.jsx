88e88025900f5ff8f658c5d3ae09df1b3c89dc06
Corey Gumbs <5513859+CoreyGumbs@users.noreply.github.com>
2019-11-19 19:46:55 -0500

    Updated Material UI components

--- airbnb/src/components/Listings/ViewAllListings.jsx
+++ airbnb/src/components/Listings/ViewAllListings.jsx
@@ -1,6 +1,6 @@
 import React, {useState, useEffect} from "react";
-// import {Link} from "react-router-dom";
-import {Button, Card, Typography, Link } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import {Box, Button, Card, CardContent, CardActions, Typography, Link } from '@material-ui/core';
 import AxiosWithAuth from "../../utils/AxiosWithAuth";
 
 const ViewAllListings = (props) => {
@@ -8,36 +8,38 @@ const ViewAllListings = (props) => {
   // Grabs all of the listings that are available from the data end points.
   useEffect(() => {
     AxiosWithAuth().get('api/listings/').then(response => {
-      console.log(response.data);
       setListings(response.data);
     })
     .catch(error => console.log(error));
    
   }, []);
-  // Need to map over all of the listings here
+ 
   return (
-    <div>
-      <h1>Current Listings</h1>
+    <Box>
+      <Typography variant="h1">Current Listings</Typography>
       {
         listings.map(listing => (
           <Card key={listing.id}>
+            <CardContent>
             <Link onClick={() => props.history.push(`/listing/${listing.id}`)}>
               <Typography variant="h3">
                 {listing.property_name}
               </Typography>
+            </Link>
+            <Link onClick={() => props.history.push(`/listing/${listing.id}`)}>
               <Typography variant="h4">{listing.neighborhood}</Typography>
             </Link>
-            <div>
+            </CardContent>
+            <CardActions>
               <Link onClick={() => props.history.push(`/listing/${listing.id}/edit`)}>
                 <Button variant="contained" color="primary">Edit</Button>
               </Link>
               <Button variant="contained" color="secondary">Delete</Button>
-            </div>
-          
+            </CardActions>
           </Card>     
         ))
       }
-    </div>
+    </Box>
 
   );
 };
